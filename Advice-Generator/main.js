7770667e5be8db6e09f7580285a737c3bd02328c
Dustin-11 <[11kurtzd@gmail.com]>
2023-08-12 16:22:37 -0400

    Active state added

--- Advice-Generator/main.js
+++ Advice-Generator/main.js
@@ -1,1 +1,13 @@
 import './style.css'
+
+const Button = document.getElementById("button");
+const TextInput = document.getElementById("text__input");
+
+Button.addEventListener("click", quote)
+
+function quote() {
+fetch("https://api.adviceslip.com/advice")
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+  TextInput.innerHTML = JSON.parse(json);
+}
--- Advice-Generator/tailwind.config.js
+++ Advice-Generator/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#999999'
+      },
+      boxShadow: {
+        'custom': '0px 0px 15px 5px rgba(255, 255, 190, .75)'
+      },
+    },
+//    colors: {
+     // div__background: '#61616b',
+  //  }
   },
   plugins: [],
 }
