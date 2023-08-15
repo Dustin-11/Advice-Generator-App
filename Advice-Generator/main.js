835f9ad9875199ef3ad246895624d0fd17835eee
Dustin-11 <[11kurtzd@gmail.com]>
2023-08-15 17:34:39 -0400

    Added functionality and @media queries

--- Advice-Generator/main.js
+++ Advice-Generator/main.js
@@ -2,12 +2,27 @@ import './style.css'
 
 const Button = document.getElementById("button");
 const TextInput = document.getElementById("text__input");
+const AdviceNumber = document.getElementById("advice__input")
+Button.addEventListener("click", getText)
 
-Button.addEventListener("click", quote)
+async function getText(file) {
+    let myObject = await fetch("https://api.adviceslip.com/advice");
+    let myText = await myObject.json();
+    TextInput.innerHTML = myText.slip.advice;
+    AdviceNumber.innerHTML = myText.slip.id;
+  }
 
+/*
 function quote() {
 fetch("https://api.adviceslip.com/advice")
   .then((response) => response.json())
-  .then((json) => console.log(json));
-  TextInput.innerHTML = JSON.parse(json);
-}
+  .then((data) => 
+  TextInput.innerHTML = data.slip.advice
+  )
+} */
+ // window.localStorage.setItem('value', JSON.stringify(data)))
+// }
+//let input = window.localStorage.getItem('value');
+//console.log(input.slip.id);
+//console.log(JSON.parse(input.slip));
+//console.log(input.slip.advice);
